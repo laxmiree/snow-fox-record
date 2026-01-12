71282f2e9f6df9b62883beb96cd43df5cb86acbd
laxmiree <131357840+laxmiree@users.noreply.github.com>
2026-01-13 00:13:12 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ favicon: 'img/favicon.ico',
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://laxmiree.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/snow-fox-record/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'laxmiree', // Usually your GitHub org/user name.
+  projectName: 'snow-fox-record', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
